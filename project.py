357c521763ffffa9b5a9f08c4521f4f0825d9a82
acarrillo889 <ADRIANCARRILLO889@GMAIL.COM>
2023-05-03 23:46:59 -0700

    Argument Exception
    Added an exception if the parameters are inputted incorrectly and exits
    Prints out how the arguments should be passed

--- project.py
+++ project.py
@@ -2,11 +2,9 @@ import praw
 import json
 import sys
 
-
-# Fill in your own stuff here
-reddit = praw.Reddit(client_id='ID',
-                    client_secret='Secret',
-                    user_agent='Agent')
+if len(sys.argv) != 2:
+    print("Usage: python script.py <subreddit limit query>")
+    sys.exit(1)
 
 subreddit_input = sys.argv[1]
 subreddit_list = subreddit_input.split()
@@ -17,6 +15,13 @@ for i in range(0, len(subreddit_list), 3):
     limit = int(subreddit_list[i+1])
     search_query = subreddit_list[i+2]
     subreddits.append((subreddit_name, limit, search_query))
+
+
+
+# Fill in your own stuff here
+reddit = praw.Reddit(client_id='ID',
+                    client_secret='Secret',
+                    user_agent='Agent')
 
 data = {"posts": []}
 
