48bcab96c3f399abf22e4949eda26d96a1c340ed
acarrillo889 <ADRIANCARRILLO889@GMAIL.COM>
2023-05-05 20:31:16 -0700

    Changed to multithreading to multipooling
    Multipooling allowed for a faster processing for the task given to it

--- crawler.bat
+++ crawler.bat
@@ -1,5 +1,5 @@
 @echo off
-set /p subreddit_input=Enter subreddits, limits, post queries, and comment queries (e.g. dog 5 "dog" "dog" amex 5 "plat" "plat"):
+set /p subreddit_input=Enter subreddits, limits, post queries, and comment queries (e.g. boba 5 "tea" "amazing" Porsche 5 "new" "drive"):
 REM Sort by relevance (default) 
 REM Sort by hot 
 REM Sort by new 
