37458084af16e375ee2efd2d888ef525e9756c67
ELW123 <ethanwan5@gmail.com>
2023-06-05 11:54:05 -0700

    Added ranking function to index.py
    ranking function needs a timestamp field, need to modify project.py

--- indexer.py
+++ indexer.py
@@ -43,6 +43,7 @@ def create_index(dir):
             doc.add(Field("permalink", post["permalink"], field_type))
             for i, comment in enumerate(post["comments"]):
                 doc.add(Field("comment_" + str(i+1), comment, field_type))
+            doc.add(Field("timestamp", post["timestamp"], field_type))
             writer.addDocument(doc)
 
     writer.commit()
@@ -56,15 +57,31 @@ def retrieve(storedir, query):
     parser = QueryParser('Context', analyzer)
     parsed_query = parser.parse(query)
     topDocs = searcher.search(parsed_query, 10).scoreDocs
-    topkdocs = []
-    for hit in topDocs:
-        doc = searcher.doc(hit.doc)
-        topkdocs.append({
-            "score": hit.score,
-            "text": doc.get("Context")
-        })
-    print(topkdocs)
+    ranked_docs = rank_documents(top_docs, searcher)
+    display_results(ranked_docs, searcher)
+    
+# should not overflow due to max_time_seconds
+def rank_documents(docs: [ScoreDoc], searcher: IndexSearcher):
+    ranked_docs = []
+    max_time_seconds = sys.maxsize / 3600  # Maximum number of seconds that won't cause overflow
+    for doc in docs:
+        lucene_doc = searcher.doc(doc.doc)
+        timestamp_str = lucene_doc.get("timestamp")
+        timestamp = datetime.strptime(timestamp_str, "%Y-%m-%d %H:%M:%S")
+        upvotes = int(lucene_doc.get("upvotes"))
+        time_elapsed_seconds = (datetime.now() - timestamp).total_seconds()
+        time_elapsed_scaled = min(time_elapsed_seconds, max_time_seconds)
+        relevance = upvotes + (1 / (time_elapsed_scaled / 3600))
+        ranked_docs.append({"score": doc.score, "doc": lucene_doc, "relevance": relevance})
+    ranked_docs.sort(key=lambda x: x["relevance"], reverse=True)
+    return ranked_docs
 
+def display_results(docs, searcher):
+    for doc in docs:
+        print("Score:", doc["score"])
+        print("Title:", doc["doc"].get("title"))
+        print("Relevance:", doc["relevance"])
+        print("-----------")
 
 index_dir = "index"
 query = 'web data'
